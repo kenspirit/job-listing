abb52278ce57e48e1a7e3c1c923d596d36a65ee2
Ken Chen <chengusky@gmail.com>
2016-08-02 12:05:37 +0800

    Add Resume and Upload attachment function

--- app/models/job.rb
+++ app/models/job.rb
@@ -21,6 +21,8 @@ class Job < ApplicationRecord
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
 
+  has_many :resumes
+
   def hidden?
     is_hidden
   end
